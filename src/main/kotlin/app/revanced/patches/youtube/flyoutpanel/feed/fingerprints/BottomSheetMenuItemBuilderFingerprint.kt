388df974495601ae22dae9c118426fcf1ec3981a
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-03-20 01:48:31 +0900

    fix(YouTube/Hide feed flyout panel): apply fingerprints compatible with the wider version

--- src/main/kotlin/app/revanced/patches/youtube/flyoutpanel/feed/fingerprints/BottomSheetMenuItemBuilderFingerprint.kt
+++ src/main/kotlin/app/revanced/patches/youtube/flyoutpanel/feed/fingerprints/BottomSheetMenuItemBuilderFingerprint.kt
@@ -1,9 +1,12 @@
 package app.revanced.patches.youtube.flyoutpanel.feed.fingerprints
 
+import app.revanced.patcher.extensions.or
 import app.revanced.patcher.fingerprint.MethodFingerprint
+import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
 
 object BottomSheetMenuItemBuilderFingerprint : MethodFingerprint(
+    accessFlags = AccessFlags.PUBLIC or AccessFlags.FINAL,
     returnType = "L",
     parameters = listOf("L"),
     opcodes = listOf(
@@ -12,5 +15,5 @@ object BottomSheetMenuItemBuilderFingerprint : MethodFingerprint(
         Opcode.INVOKE_STATIC,
         Opcode.MOVE_RESULT_OBJECT
     ),
-    strings = listOf("ElementTransformer, ElementPresenter and InteractionLogger cannot be null")
+    strings = listOf("Text missing for BottomSheetMenuItem with iconType: ")
 )
