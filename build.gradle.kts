9f0e512283db32033e29413db7ec31f95c32a55c
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-03-22 19:05:13 +0900

    build: bump dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -102,7 +102,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/revanced/revanced-patches")
+            url = uri("https://maven.pkg.github.com/inotia00/revanced-patches")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
