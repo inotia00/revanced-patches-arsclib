12e88259e29b1a5105549c783364efcfe5f9cb73
inotia00 <108592928+inotia00@users.noreply.github.com>
2024-04-23 22:08:54 +0900

    rollback(YouTube/Player Type Hook): add view invalidate observer
    - Since concurrency issue cannot be resolved, roll back to the previous commit.

--- src/main/kotlin/app/revanced/patches/youtube/utils/playertype/PlayerTypeHookPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/utils/playertype/PlayerTypeHookPatch.kt
@@ -6,38 +6,23 @@ import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
 import app.revanced.patcher.extensions.InstructionExtensions.getInstruction
 import app.revanced.patcher.patch.BytecodePatch
 import app.revanced.patcher.patch.annotation.Patch
-import app.revanced.patches.shared.litho.LithoFilterPatch
 import app.revanced.patches.youtube.utils.fingerprints.YouTubeControlsOverlayFingerprint
-import app.revanced.patches.youtube.utils.integrations.Constants.COMPONENTS_PATH
 import app.revanced.patches.youtube.utils.integrations.Constants.SHARED_PATH
 import app.revanced.patches.youtube.utils.integrations.Constants.UTILS_PATH
 import app.revanced.patches.youtube.utils.playertype.fingerprint.ActionBarSearchResultsFingerprint
 import app.revanced.patches.youtube.utils.playertype.fingerprint.PlayerTypeFingerprint
-import app.revanced.patches.youtube.utils.playertype.fingerprint.ReelWatchPagerFingerprint
 import app.revanced.patches.youtube.utils.playertype.fingerprint.VideoStateFingerprint
 import app.revanced.patches.youtube.utils.resourceid.SharedResourceIdPatch
-import app.revanced.patches.youtube.utils.resourceid.SharedResourceIdPatch.ReelWatchPlayer
-import app.revanced.patches.youtube.utils.resourceid.SharedResourceIdPatch.SizeAdjustableYouTubeControlsOverlay
-import app.revanced.util.getTargetIndex
 import app.revanced.util.getTargetIndexWithMethodReferenceName
-import app.revanced.util.getWideLiteralInstructionIndex
 import app.revanced.util.resultOrThrow
-import com.android.tools.smali.dexlib2.Opcode
 import com.android.tools.smali.dexlib2.iface.instruction.FiveRegisterInstruction
-import com.android.tools.smali.dexlib2.iface.instruction.OneRegisterInstruction
 import com.android.tools.smali.dexlib2.iface.instruction.ReferenceInstruction
 
-@Patch(
-    dependencies = [
-        LithoFilterPatch::class,
-        SharedResourceIdPatch::class
-    ]
-)
+@Patch(dependencies = [SharedResourceIdPatch::class])
 object PlayerTypeHookPatch : BytecodePatch(
     setOf(
         ActionBarSearchResultsFingerprint,
         PlayerTypeFingerprint,
-        ReelWatchPagerFingerprint,
         YouTubeControlsOverlayFingerprint
     )
 ) {
@@ -46,9 +31,6 @@ object PlayerTypeHookPatch : BytecodePatch(
 
     private const val INTEGRATIONS_ROOT_VIEW_HOOK_CLASS_DESCRIPTOR =
         "$SHARED_PATH/RootView;"
-
-    private const val FILTER_CLASS_DESCRIPTOR =
-        "$COMPONENTS_PATH/InvalidateStateFilter;"
 
     override fun execute(context: BytecodeContext) {
 
@@ -78,32 +60,6 @@ object PlayerTypeHookPatch : BytecodePatch(
                     )
                 }
             }
-
-            parentResult.mutableMethod.apply {
-                val constIndex = getWideLiteralInstructionIndex(SizeAdjustableYouTubeControlsOverlay)
-                val targetIndex = getTargetIndex(constIndex, Opcode.MOVE_RESULT_OBJECT)
-                val targetRegister = getInstruction<OneRegisterInstruction>(targetIndex).registerA
-
-                addInstruction(
-                    targetIndex + 1,
-                    "invoke-static {v$targetRegister}, " +
-                            "$INTEGRATIONS_ROOT_VIEW_HOOK_CLASS_DESCRIPTOR->onPlayerCreate(Landroid/view/View;)V"
-                )
-            }
-        }
-
-        ReelWatchPagerFingerprint.resultOrThrow().let {
-            it.mutableMethod.apply {
-                val constIndex = getWideLiteralInstructionIndex(ReelWatchPlayer)
-                val targetIndex = getTargetIndex(constIndex, Opcode.MOVE_RESULT_OBJECT)
-                val targetRegister = getInstruction<OneRegisterInstruction>(targetIndex).registerA
-
-                addInstruction(
-                    targetIndex + 1,
-                    "invoke-static {v$targetRegister}, " +
-                            "$INTEGRATIONS_ROOT_VIEW_HOOK_CLASS_DESCRIPTOR->onShortsCreate(Landroid/view/View;)V"
-                )
-            }
         }
 
         // Hook the search bar.
@@ -122,7 +78,6 @@ object PlayerTypeHookPatch : BytecodePatch(
             )
         }
 
-        LithoFilterPatch.addFilter(FILTER_CLASS_DESCRIPTOR)
 
     }
 }
