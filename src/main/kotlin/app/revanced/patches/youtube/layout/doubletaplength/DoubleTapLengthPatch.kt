e29e910f6a03a4ce4ec525f588101d06a13339af
KobeW50 <84587632+KobeW50@users.noreply.github.com>
2024-05-10 19:22:16 -0400

    fix(YouTube/Custom double tap length): Unclear wording

--- src/main/kotlin/app/revanced/patches/youtube/layout/doubletaplength/DoubleTapLengthPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/layout/doubletaplength/DoubleTapLengthPatch.kt
@@ -14,7 +14,7 @@ import java.nio.file.Files
 @Suppress("DEPRECATION", "unused")
 object DoubleTapLengthPatch : BaseResourcePatch(
     name = "Custom double tap length",
-    description = "Adds 'double-tap to seek' values specified in options.json.",
+    description = "Adds 'double-tap to seek' values that are specified in options.json.",
     dependencies = setOf(SettingsPatch::class),
     compatiblePackages = COMPATIBLE_PACKAGE
 ) {
