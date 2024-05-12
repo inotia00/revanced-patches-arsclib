1ed48b349512648636446d3c4c2fc12755258556
KobeW50 <84587632+KobeW50@users.noreply.github.com>
2024-05-12 13:31:01 -0400

    Patch description changes (#46)
    * fix(YouTube/Video playback): Typo in patch description
    
    * feat(YouTube): Improve patch description
    
    fix(YouTube/Change shorts repeat state): Patch description issues
    
    feat(YouTube/Custom double tap length): Clarify patch description
    
    fix(YouTube/Description components): Grammar in patch description
    
    fix(YouTube/Disable haptic feedback): Patch description grammar
    
    feat(YouTube/Disable splash animation): Clarify patch description
    
    fix(YouTube/Enable gradient loading screen): Grammar
    
    fix(YouTube/Hide feed components): Grammar
    
    feat(YouTube/Layout switch): Clarify patch description
    
    fix(YouTube/MaterialYou): Add punctuation to patch description
    
    fix(YouTube/Navigation bar components): Grammar
    
    (feat(YouTube/Overlay buttons): Clarify patch description
    
    fix(YouTube/Player components): Grammar
    
    fix(YouTube/RYD): Make patch description consistent
    
    fix(YouTube/Seekbar components): Incorrect patch description
    
    fix(YouTube/SponsorBlock): Grammar
    
    fix(YouTube/Shorts outline icon): Make patch description consistent
    
    fix(YouTube/Spoof format stream data): Grammar
    
    feat(YouTube/Spoof test client): Clarify patch description
    
    fix(YouTube/Theme): Grammar
    
    fix(YouTube/Toolbar components): Punctuation
    
    fix(YouTube/Translations): Consistent grammar
    
    * fix(YouTube/Custom double tap length): Unclear wording
    
    * feat(YouTube/Custom branding icon): Use consistent patch description
    
    * feat(YouTube/Custom branding name): Use consistent patch description
    
    * feat(YouTube): Make patch descriptions consistent with YT Music
    
    * feat(YT Music): Improve patch descriptions
    
    * feat(YT Music/Hide overlay filter): Clarify patch description

--- src/main/kotlin/app/revanced/patches/youtube/utils/fix/formatstream/SpoofFormatStreamDataPatch.kt
+++ src/main/kotlin/app/revanced/patches/youtube/utils/fix/formatstream/SpoofFormatStreamDataPatch.kt
@@ -30,7 +30,7 @@ import com.android.tools.smali.dexlib2.iface.reference.MethodReference
 @Suppress("unused")
 object SpoofFormatStreamDataPatch : BaseBytecodePatch(
     name = "Spoof format stream data",
-    description = "Adds options to spoof format stream data to prevent playback issues.",
+    description = "Adds an option to spoof the format stream data to prevent playback issues.",
     dependencies = setOf(
         PlayerResponseMethodHookPatch::class,
         SettingsPatch::class,
